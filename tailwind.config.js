90856cb25b02abbcdb12c0b340294c289d55f924
bangkitwira <bangkitwira@gmail.com>
2023-08-20 15:21:00 +0700

    add cypress for e2e tests

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'boost-device-md': "url('/assets/images/boost-vector.svg')",
+        'boost-device-xs': "url('/assets/images/boost-vector-mobile.svg')",
+        'bottom-boost-md': "url('/assets/images/bottom-boost.svg')",
+        'bottom-boost-xs': "url('/assets/images/bottom-boost-mobile.svg')"
+      })
+    }
   },
   plugins: [],
 }
