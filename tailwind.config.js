326a257a39da740f774165d5812b1ccac0c3d481
bangkitwira <bangkitwira@gmail.com>
2023-08-22 12:23:04 +0700

    update assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,10 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: theme => ({
-        'boost-device-md': "url('/assets/images/boost-vector.svg')",
-        'boost-device-xs': "url('/assets/images/boost-vector-mobile.svg')",
-        'bottom-boost-md': "url('/assets/images/bottom-boost.svg')",
-        'bottom-boost-xs': "url('/assets/images/bottom-boost-mobile.svg')"
+        'boost-device-md': "url('./assets/images/boost-vector.svg')",
+        'boost-device-xs': "url('./assets/images/boost-vector-mobile.svg')",
+        'bottom-boost-md': "url('./assets/images/bottom-boost.svg')",
+        'bottom-boost-xs': "url('./assets/images/bottom-boost-mobile.svg')"
       })
     }
   },
